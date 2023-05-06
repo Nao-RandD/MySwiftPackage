7242a815ad3a1246a988cd6d8e110044a50057c6
Nao-RandD <nao.r.and.d@gmail.com>
2023-05-07 08:21:57 +0900

    first commit! :tada:

---
+++ Sources/MySwiftPackage/MySwiftPackage.swift
@@ -0,0 +1,10 @@
+public struct MySwiftPackage {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+
+    public static func hello() {
+        print("Hello, world!")
+    }
+}
